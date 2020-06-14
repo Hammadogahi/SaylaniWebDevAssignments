d003597be99686fe432470c0085f5cc54ee40de5
Hammad <hammadx11@gmail.com>
2020-06-14 19:51:50 +0500

    JS Assingment

---
+++ Assignment no.5/java-test.js
@@ -0,0 +1,8 @@
+// document.write("<h1>Rules for naming JS varaibles</h1><br><br>" + "Variable names can contain, numbers, $ and _. For example: $my_1stVariable" + "<br>Variable must behin with a letter, $ or _. For example : $name, _name or name" + "\n" + "Variable names are case sensitive" + "<br>Variable names should not be JS keywords")
+
+var num1 = prompt ("Please enter the first value")
+var num2 = prompt ("Please enter the second value")
+
+
+result= (num1) - (num2);
+alert (result)
